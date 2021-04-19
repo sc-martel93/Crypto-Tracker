69decf46b85be7fa7ed77bb312b3617fb8a71fc2
Scott Martel <sc.martel93@gmail.com>
2021-04-19 15:59:58 -0400

    Set up useEffect API call to coin geck

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import react, { useEffect, useState } from 'react'
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    axios
+      .get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+      .then((res) => {
+        setCoins(res.data)
+        console.log(res.data)
+      })
+      .catch(err => console.error(err))
+  }, [])
+
   return (
     <div className="App">
       <div className="header">
