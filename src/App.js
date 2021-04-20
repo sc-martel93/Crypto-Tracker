5d18cadf1c86668f36c209c891304cc5f10e5288
Scott Martel <sc.martel93@gmail.com>
2021-04-20 00:36:41 -0400

    finalized coin component and added search functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import react, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios';
 import './App.css';
 import Coin from './components/Coin';
@@ -6,6 +6,7 @@ import Coin from './components/Coin';
 function App() {
 
   const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState("")
 
   useEffect(() => {
     axios
@@ -17,18 +18,38 @@ function App() {
       .catch(err => console.error(err))
   }, [])
 
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
     <div className="App">
       <div className="header">
-        <h1 className="brand"><i class="fas fa-coins"></i>Crypto Tracker</h1>
+        <h1 className="brand"><i className="fas fa-coins"></i>Crypto Tracker</h1>
         <form>
-          <input className="inputField" type="text" placeholder="Search a Coin"></input>
+          <input className="inputField" type="text" onChange={handleChange} placeholder="Search a Coin"></input>
         </form>
       </div>
       <div className="coinsContainer">
-        <Coin />
+        {filteredCoins.map((coin) => {
+          return (
+            <Coin
+              key={coin.id}
+              name={coin.name}
+              price={coin.current_price}
+              symbol={coin.symbol}
+              marketCap={coin.market_cap}
+              volume={coin.total_volume}
+              image={coin.image}
+              priceChange={coin.price_change_percentage_24h}
+            />
+          );
+        })}
       </div>
-
     </div>
   );
 }
--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,20 +1,36 @@
 import React from 'react'
+import "./Coin.css"
 
-function Coin() {
+const Coin = ({ name, price, symbol, marketCap, volume, image, priceChange }) => {
     return (
         <div className="cryptoCoin">
-            <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" alt="Bitcoin" className="coinLogo" />
+            <img src={image} alt={`${name}`} className="coinLogo" />
             <div className="coinNameWrap">
-                <h1 className="coinName">BitCoin</h1>
-                <p className="coinSymbol">BTC</p>
+                <h1 className="coinName">{name}</h1>
+                <p className="coinSymbol">{symbol}</p>
             </div>
-            <p className="coinPrice">$60,000</p>
+            <p className="coinPrice">${price.toLocaleString()}</p>
             <p className="coinMarketCap">
-                Market Cap: $1T
+                Market Cap: ${marketCap.toLocaleString()}
             </p>
             <p className="coinVolume">
-                Volume (24H): 69,000,000
+                Volume (24H): ${volume.toLocaleString()}
             </p>
+            {priceChange < 0 ? (
+                <div className="priceContainerDOWN">
+                    <i className="fas fa-angle-down fa-2x"></i>
+                    <p className="priceChange">
+                        {priceChange.toFixed(2)}
+                    </p>
+                </div>
+            ) : (
+                <div className="priceContainerUP">
+                    <i className="fas fa-angle-up fa-2x"></i>
+                    <p className="priceChange">
+                        {priceChange.toFixed(2)}
+                    </p>
+                </div>
+            )}
         </div>
     )
 }
